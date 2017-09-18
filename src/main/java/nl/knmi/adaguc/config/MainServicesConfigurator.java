edd09eeb9358d67a420f38f6c5eac87a3226636a
maartenplieger <maarten.plieger@knmi.nl>
2017-09-18 16:02:58 +0200

    Initial commit

---
+++ src/main/java/nl/knmi/adaguc/config/MainServicesConfigurator.java
@@ -0,0 +1,46 @@
+package nl.knmi.adaguc.config;
+
+import nl.knmi.adaguc.tools.ElementNotFoundException;
+import nl.knmi.adaguc.tools.MyXMLParser.XMLElement;
+
+/**
+<?xml version="1.0" encoding="UTF-8"?>
+<adaguc-services>
+  <external-home-url>http://172.17.0.2:8080</external-home-url>
+</adaguc-services>
+*/
+
+public class MainServicesConfigurator implements ConfiguratorInterface {
+	private static String serverExternalURL="";
+	private static String userWorkspace="/tmp";
+	private static String serverPort="443";
+	private static String baseDir="/tmp/";
+
+	public void doConfig(XMLElement configReader) throws ElementNotFoundException {
+		serverExternalURL = configReader.getNodeValueMustNotBeUndefined("adaguc-services.external-home-url");
+		userWorkspace = configReader.getNodeValueMustNotBeUndefined("adaguc-services.userworkspace");
+		serverPort = configReader.getNodeValue("adaguc-services.server.port");
+		baseDir = configReader.getNodeValueMustNotBeUndefined("adaguc-services.basedir");
+	}
+
+	public static String getServerExternalURL() throws ElementNotFoundException {
+		ConfigurationReader.readConfig();
+		return serverExternalURL;
+	}
+	
+	public static String getUserWorkspace() throws ElementNotFoundException{
+		ConfigurationReader.readConfig();
+		return userWorkspace;
+	}
+
+	public static String getServerPort() throws ElementNotFoundException {
+		ConfigurationReader.readConfig();
+		return serverPort;
+		
+	}
+
+	public static String getBaseDir() throws ElementNotFoundException {
+		ConfigurationReader.readConfig();
+		return baseDir;
+	}
+}
